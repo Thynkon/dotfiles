05166a4caebf267614850f901adfdde262647134
Mário Ferreira <marioferreira2110@gmail.com>
2020-04-14 22:37:20 +0200

    Add polybar config

---
+++ private_dot_config/private_polybar/private_executable_launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Display polybar on multiple monitors
+# Reference: https://github.com/polybar/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		echo "m is ===$m==="
+		MONITOR=$m polybar -c ~/.config/polybar/config.ini top & polybar -c ~/.config/polybar/config.ini bottom
+	done
+else
+   polybar -c ~/.config/polybar/config.ini top & polybar -c ~/.config/polybar/config.ini bottom &
+fi
