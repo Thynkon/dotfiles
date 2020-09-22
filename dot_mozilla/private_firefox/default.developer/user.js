309cce0fce5b152ada85e84e3c612ba0d41a7dae
thynkon <thynkon@protonmail.com>
2020-09-22 21:23:00 +0200

    Enable firefox pdf viewer

--- dot_mozilla/private_firefox/default.developer/user.js
+++ dot_mozilla/private_firefox/default.developer/user.js
@@ -554,7 +554,7 @@ user_pref("browser.startup.blankWindow", false);
 // https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2015-2743
 // https://blog.mozilla.org/security/2015/08/06/firefox-exploit-found-in-the-wild/
 // https://www.mozilla.org/en-US/security/advisories/mfsa2015-69/
-user_pref("pdfjs.disabled",					true);
+user_pref("pdfjs.disabled",					false);
 
 // PREF: Disable collection/sending of the health report (healthreport.sqlite*)
 // https://support.mozilla.org/en-US/kb/firefox-health-report-understand-your-browser-perf
