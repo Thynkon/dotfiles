6ef8902005c884c1b4b082522440ef739b547a7f
thynkon <thynkon@protonmail.com>
2021-07-12 09:55:16 +0200

    Update telescope config file to newest version

--- private_dot_config/private_nvim/lua/plugin/telescope.lua
+++ private_dot_config/private_nvim/lua/plugin/telescope.lua
@@ -9,23 +9,20 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-    prompt_position = "bottom",
     prompt_prefix = ">",
     selection_strategy = "reset",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
-    layout_defaults = {
-      -- TODO add builtin options.
+    layout_config = {
+      width = 0.75,
+      prompt_position = "bottom",
+      preview_cutoff = 120,
     },
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    shorten_path = true,
+    display_path = true,
     winblend = 0,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰'},
     color_devicons = true,
