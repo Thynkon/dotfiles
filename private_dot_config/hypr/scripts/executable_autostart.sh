75b559f20adf7070d864552858f02d3d7dae5a7e
Thynkon <thynkon@protonmail.com>
2024-10-04 18:45:53 +0200

    Add hyprland config files

---
+++ private_dot_config/hypr/scripts/executable_autostart.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+run() {
+  if ! pgrep $1; then
+    $@ &
+  fi
+}
+
+# email
+run protonmail-bridge --no-window
+
+run nextcloud
+# Wifi applet
+run nm-applet
+
+# Blue light filter
+run redshift
+
+run onedrivegui
+
+# Device manager
+run udiskie
+
+# run "numlockx on"
+
+run gpg-agent
+
+run waybar
+run hyprpaper
+run blueman-applet
+
+run eww daemon -c $HOME/.config/eww-which-key
+run hyprpm reload -n
+run hyprpm update -v
+
+exit 0
