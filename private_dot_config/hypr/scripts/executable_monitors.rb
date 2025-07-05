5571206e4d3b3b112710e4eff07c457754e1497b
Thynkon <thynkon@protonmail.com>
2025-07-05 12:35:01 +0200

    Fix hyprland monitors.rb script

--- private_dot_config/hypr/scripts/executable_monitors.rb
+++ private_dot_config/hypr/scripts/executable_monitors.rb
@@ -1,26 +1,62 @@
 #!/usr/bin/env ruby
 require 'json'
 
-# Get monitor information
-monitors_json = `hyprctl monitors -j`
-monitors = JSON.parse(monitors_json)
+def get_monitors
+  # Get monitor information
+  monitors_json = `hyprctl monitors -j`
+  JSON.parse(monitors_json)
+end
 
-laptop = 'eDP-1'
-laptop_res = 0
+def extract_res_freq(mode)
+  res, freq = mode.split('@')
+  freq = freq.sub('Hz', '').to_i
 
-monitors.each do |monitor|
-  name = monitor['name']
-  max_freq = monitor['availableModes'].max_by do |mode|
-    res, freq = mode.split('@')
-    laptop_res = res if name == laptop
-    freq.sub('Hz', '').to_i
+  {
+    "res": res,
+    "freq": freq
+  }
+end
+
+def get_main_monitor_res(monitors, main_monitor)
+  monitor = monitors.find { |m| m['name'] == main_monitor }
+
+  mode = monitor['availableModes'].max_by do |mode|
+    extract_res_freq(mode)[:freq]
   end
 
-  width = laptop_res.split('x').first
+  extract_res_freq(mode)[:res]
+end
 
-  if name == laptop
-    system("hyprctl keyword monitor \"#{name},#{max_freq}, 0x0, 1\"")
+def exec_cmd(cmd, debug_mode)
+  if debug_mode
+    puts(cmd)
   else
-    system("hyprctl keyword monitor \"#{name},#{max_freq}, #{width}x0, 1\"")
+    system(cmd)
   end
 end
+
+def setup_monitors(debug_mode: false)
+  monitors = get_monitors
+
+  main_monitor = 'eDP-1'
+  laptop_mode = monitors.any? { |monitor| monitor['name'] == main_monitor }
+  main_monitor = 'HDMI-A-1' unless laptop_mode
+  main_monitor_res = get_main_monitor_res(monitors, main_monitor)
+
+  monitors.each do |monitor|
+    name = monitor['name']
+    max_freq = monitor['availableModes'].max_by { |mode| extract_res_freq(mode)[:freq] }
+
+    width = main_monitor_res.split('x').first
+
+    cmd = if name == main_monitor
+            "hyprctl keyword monitor \"#{name},#{max_freq}, 0x0, 1\""
+          else
+            "hyprctl keyword monitor \"#{name},#{max_freq}, #{width}x0, 1\""
+          end
+
+    exec_cmd(cmd, debug_mode)
+  end
+end
+
+setup_monitors(debug_mode: true)
